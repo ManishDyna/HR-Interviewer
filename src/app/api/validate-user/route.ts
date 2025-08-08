cc4a334e1877d99b889a64212be5e88872d8b16a
Manish Soni <Manish.Soni@dynatechconsultancy.com>
2025-08-08 10:46:35 +0530

    First Commit

---
+++ src/app/api/validate-user/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY   !;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email } = await req.json();
+    console.log("Received email:", email);
+    if (!email) {
+      return NextResponse.json({ error: "Email is required" }, { status: 400 });
+    }
+    const { data, error } = await supabase
+      .from("user")
+      .select("id")
+      .ilike("email", email)
+      .single();
+    console.log("Supabase data:", data, "error:", error);
+    if (error || !data) {
+      return NextResponse.json(
+        { error: "You are not authorized person" },
+        { status: 401 }
+      );
+    }
+    return NextResponse.json({ success: true }, { status: 200 });
+  } catch (err) {
+    console.error("Server error:", err);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
